eae2a1d3136445cf80a581242861c680f26b132d
alex <alex@qr.com>
2018-12-01 01:17:18 +0800

    第一个三角形 2-3

--- src/main.cpp
+++ src/main.cpp
@@ -10,73 +10,31 @@ FreeTypeFont*	_font43;
 int _width = 800;
 int _height = 600;
 
-
-struct Vertex
-{
-    unsigned char r,g,b,a;
-    float x, y, z;
-};
-
-Vertex cubeVertices[] =
-{
-    { 255,0,0,255, -1.0f,-1.0f, 1.0f },
-    { 255,0,0,255, 1.0f,-1.0f, 1.0f },
-    { 255,0,0,255, 1.0f, 1.0f, 1.0f },
-    { 255,0,0,255, -1.0f, 1.0f, 1.0f },
-
-    { 0,255,0,255, -1.0f,-1.0f,-1.0f },
-    { 0,255,0,255, -1.0f, 1.0f,-1.0f },
-    { 0,255,0,255, 1.0f, 1.0f,-1.0f },
-    { 0,255,0,255, 1.0f,-1.0f,-1.0f },
-
-    { 0,0,255,255, -1.0f, 1.0f,-1.0f },
-    { 0,0,255,255, -1.0f, 1.0f, 1.0f },
-    { 0,0,255,255, 1.0f, 1.0f, 1.0f },
-    { 0,0,255,255, 1.0f, 1.0f,-1.0f },
-
-    { 0,255,255,255, -1.0f,-1.0f,-1.0f },
-    { 0,255,255,255, 1.0f,-1.0f,-1.0f },
-    { 0,255,255,255, 1.0f,-1.0f, 1.0f },
-    { 0,255,255,255, -1.0f,-1.0f, 1.0f },
-
-    { 255,0,255,255, 1.0f,-1.0f,-1.0f },
-    { 255,0,255,255, 1.0f, 1.0f,-1.0f },
-    { 255,0,255,255, 1.0f, 1.0f, 1.0f },
-    { 255,0,255,255, 1.0f,-1.0f, 1.0f },
-
-    { 255,255,255,255, -1.0f,-1.0f,-1.0f },
-    { 255,255,255,255, -1.0f,-1.0f, 1.0f },
-    { 255,255,255,255, -1.0f, 1.0f, 1.0f },
-    { 255,255,255,255, -1.0f, 1.0f,-1.0f }
-};
-
-
+#define M_PI (3.14159265358979323846)
 
 void myDisplay(void)
 {
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glViewport(0,0,_width,_height);
+
 #if 1
+	 //! 指定以下的操作针对投影矩阵
 	glMatrixMode(GL_PROJECTION);
+	//! 将投影举证清空成单位矩阵
 	glLoadIdentity();
-	gluPerspective(60,double(_width)/double(_height),0.1,1000);
+	glOrtho(0,_width,_height,0,-100,100);
 
-	glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-	glTranslatef(0,0,-3);
-	 	
-	//glEnable(GL_DEPTH_TEST);
-	glEnableClientState(GL_VERTEX_ARRAY);
-	glEnableClientState(GL_COLOR_ARRAY);
-	glVertexPointer(3,GL_FLOAT,         sizeof(Vertex),     &cubeVertices[0].x);
-	glColorPointer(4,GL_UNSIGNED_BYTE,  sizeof(Vertex),     cubeVertices);
+	glColor3f(1,1,1);
+	glBegin(GL_TRIANGLES);
 
-	glDrawArrays( GL_QUADS, 0, 24 );
+		glVertex3f(_width * 0.5f,0,0);
+		glVertex3f(_width,_height,0);
+		glVertex3f(0,_height,0);
+
+	glEnd();
 #endif
-	_font->begin(_width,_height);
-	_font->drawText(10,100,0,Rgba(125,255,255),L"abc123这边就是可以",0,0,0);
-	_font->end();
+
 
 	glutSwapBuffers();
 }
@@ -88,15 +46,6 @@ int main(int argc, char *argv[])
     glutInitWindowPosition(100, 100);
     glutInitWindowSize(_width, _height);
     glutCreateWindow("123456789");    // 改了窗口标题
-
-    _font   =   new FreeTypeFont();
-    _font->create("simsun.ttc",16,512,512);
-
-    _font43 =   new FreeTypeFont();
-    _font43->create("simsun.ttc",40,512,512);
-
-
-
 
     glutDisplayFunc(&myDisplay);
     glutMainLoop();
