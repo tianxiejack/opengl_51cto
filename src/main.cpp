9af3fdb1ac0d9d8bd185c4f36fa4a10e0efcd2da
alex <alex@qr.com>
2018-12-01 01:42:08 +0800

    三角形列表绘制圆 2-3

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,14 @@ int _height = 600;
 
 #define M_PI (3.14159265358979323846)
 
+typedef struct float3
+{
+    float   x,y,z;
+    float   w;
+}float3_t;
+
+float3_t  _circle[360*3];
+
 void myDisplay(void)
 {
 
@@ -19,20 +27,36 @@ void myDisplay(void)
 	glViewport(0,0,_width,_height);
 
 #if 1
-	 //! 指定以下的操作针对投影矩阵
-	glMatrixMode(GL_PROJECTION);
-	//! 将投影举证清空成单位矩阵
-	glLoadIdentity();
-	glOrtho(0,_width,_height,0,-100,100);
+       //! 指定以下的操作针对投影矩阵
+       glMatrixMode(GL_PROJECTION);
+       //! 将投影举证清空成单位矩阵
+       glLoadIdentity();
+       glOrtho(0,_width,_height,0,-100,100);
 
-	glColor3f(1,1,1);
-	glBegin(GL_TRIANGLES);
+       float   cx  =   100;
+       float   cy  =   100;
+       float   cz  =   0;
+       float   r   =   80;
 
-		glVertex3f(_width * 0.5f,0,0);
-		glVertex3f(_width,_height,0);
-		glVertex3f(0,_height,0);
+       glColor3f(1,1,1);
 
-	glEnd();
+       for (int i = 0 ;i < 360 ; ++ i)
+       {
+           _circle[i * 3 + 0].x    =  cx;
+           _circle[i * 3 + 0].y    =  cy;
+           _circle[i * 3 + 0].z    =  cz;
+
+           _circle[i * 3 + 1].x    =  (float)cos((double)i * M_PI/180) * r + cx;
+           _circle[i * 3 + 1].y    =  (float)sin((double)i * M_PI/180) * r + cy;
+           _circle[i * 3 + 1].z    =  cz;
+
+           _circle[i * 3 + 2].x    =  (float)cos((double)(i + 1) * M_PI/180) * r + cx;
+           _circle[i * 3 + 2].y    =  (float)sin((double)(i + 1) * M_PI/180) * r + cy;
+           _circle[i * 3 + 2].z    =  cz;
+       }
+       glEnableClientState(GL_VERTEX_ARRAY);
+       glVertexPointer(3,GL_FLOAT,sizeof(float3_t),_circle);
+       glDrawArrays(GL_TRIANGLES,0,360 * 3);
 #endif
 
 
