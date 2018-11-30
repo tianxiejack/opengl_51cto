2d58d4f4b54fa545adde7cfb11d92a1a5ebd46ae
alex <alex@qr.com>
2018-12-01 01:55:29 +0800

    带颜色的三角形绘制 2-7

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,11 @@ int _height = 600;
 
 #define M_PI (3.14159265358979323846)
 
-typedef struct
+struct  Vertex
 {
-    float   x,y,z;
-}float3_t;
-
-float3_t  _circle[362];
+   float   x,y,z;
+   float   r,g,b;
+};
 
 void myDisplay(void)
 {
@@ -26,25 +25,47 @@ void myDisplay(void)
 	glViewport(0,0,_width,_height);
 
 #if 1
-       //! 指定以下的操作针对投影矩阵
-       glMatrixMode(GL_PROJECTION);
-       //! 将投影举证清空成单位矩阵
-       glLoadIdentity();
-       glOrtho(0,_width,_height,0,-100,100);
+//! 指定以下的操作针对投影矩阵
+glMatrixMode(GL_PROJECTION);
+//! 将投影举证清空成单位矩阵
+glLoadIdentity();
+glOrtho(0,_width,_height,0,-100,100);
 
-       glColor3f(1,1,1);
 
-       float3_t  rect[]  =
-	   {
-		   {10,    10,     0},
-		   {110,   10,     0},
-		   {10,    110,    0},
-		   {110,   110,    0},
-	   };
+glColor3f(1,0,1);
 
-	   glEnableClientState(GL_VERTEX_ARRAY);
-	   glVertexPointer(3,GL_FLOAT,sizeof(float3_t),rect);
-	   glDrawArrays(GL_TRIANGLE_STRIP,0,4);
+Vertex  rect[]  =
+{
+    {10,    10,     0,  1,  0,  0},
+    {110,   10,     0,  0,  1,  0},
+    {10,    110,    0,  0,  0,  1},
+    {110,   110,    0,  1,  0,  1},
+};
+glColor3f(1,0,1);
+glEnableClientState(GL_VERTEX_ARRAY);
+glEnableClientState(GL_COLOR_ARRAY);
+glVertexPointer(3,GL_FLOAT,sizeof(Vertex),rect);
+glColorPointer(3,GL_FLOAT,sizeof(Vertex),&rect[0].r);
+glDrawArrays(GL_TRIANGLE_STRIP,0,4);
+
+
+Vertex  rect1[]  =
+{
+    {10,    10,     0,  1,  0,  0},
+    {110,   10,     0,  0,  1,  0},
+    {10,    110,    0,  0,  0,  1},
+    {110,   110,    0,  1,  0,  1},
+};
+
+for (int i = 0 ;i < 4 ; ++ i )
+{
+    rect1[i].x += 100;
+}
+
+glVertexPointer(3,GL_FLOAT,sizeof(Vertex),rect1);
+glColorPointer(3,GL_FLOAT,sizeof(Vertex),&rect1[0].r);
+glColor3f(0,0,1);
+glDrawArrays(GL_TRIANGLE_STRIP,0,4);
 #endif
 
 
